2def54b85c7f216ec4bde90236dc80a7a5df009b
Comparitiko <gabigcy@gmail.com>
2024-09-08 12:40:26 +0200

    Antes de ssr activado en pokemon-ssr

---
+++ angular-pro/pokemon-ssr/src/app/pages/pricing-page/pricing-page.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'pricing-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './pricing-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PricingPageComponent {
+
+}
